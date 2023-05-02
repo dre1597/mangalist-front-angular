ef19d5477d89224692945152311db05a19326a77
André Pereira <andrepereira97@yahoo.com>
2023-05-01 21:50:08 -0300

    :recycle: :sparkles: change material to bootstrap and add validation to signup form

--- src/app/pages/signup/components/signup-form/signup-form.component.ts
+++ src/app/pages/signup/components/signup-form/signup-form.component.ts
@@ -1,20 +1,98 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+
+type FormFields = {
+  name: AbstractControl<string | null>;
+  email: AbstractControl<string | null>;
+  password: AbstractControl<string | null>;
+  confirmPassword: AbstractControl<string | null>;
+};
+
+const MIN_LENGTH_NAME = 3;
+const MAX_LENGTH_NAME = 50;
+const MIN_LENGTH_EMAIL = 3;
+const MAX_LENGTH_EMAIL = 50;
+const MIN_LENGTH_PASSWORD = 6;
+const MAX_LENGTH_PASSWORD = 50;
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.scss'],
 })
-export class SignupFormComponent {
-  form: FormGroup = new FormGroup({
-    email: new FormControl(''),
-    name: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+export class SignupFormComponent implements OnInit {
+  form: FormGroup<FormFields> = new FormGroup<FormFields>({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(MIN_LENGTH_NAME),
+      Validators.maxLength(MAX_LENGTH_NAME),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(MIN_LENGTH_EMAIL),
+      Validators.maxLength(MAX_LENGTH_EMAIL),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(MIN_LENGTH_PASSWORD),
+      Validators.maxLength(MAX_LENGTH_PASSWORD),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(MIN_LENGTH_PASSWORD),
+      Validators.maxLength(MAX_LENGTH_PASSWORD),
+    ]),
   });
 
-  handleSubmit(): void {
-    console.log(this.form.value);
+  get name(): AbstractControl<string | null> {
+    return this.form.get('name') as AbstractControl;
   }
+
+  get email(): AbstractControl<string | null> {
+    return this.form.get('email') as AbstractControl;
+  }
+
+  get password(): AbstractControl<string | null> {
+    return this.form.get('password') as AbstractControl;
+  }
+
+  get confirmPassword(): AbstractControl<string | null> {
+    return this.form.get('confirmPassword') as AbstractControl;
+  }
+
+  ngOnInit(): void {
+    this._addMatchValidator();
+  }
+
+  handleSubmit(): void {
+    console.log(this.form?.value);
+  }
+
+  private _addMatchValidator(): void {
+    this.form.addValidators(
+      matchValidator(
+        this.form.get('password') as FormControl,
+        this.form.get('confirmPassword') as FormControl
+      )
+    );
+  }
+}
+
+function matchValidator(
+  control: AbstractControl,
+  controlTwo: AbstractControl
+): ValidatorFn {
+  return () => {
+    if (control.value !== controlTwo.value) {
+      return { match_error: true };
+    }
+    return null;
+  };
 }
--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignupPageComponent } from './signup-page.component';
-import { MaterialModule } from '../../material.module';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 import { SignupRoutingModule } from './signup-routing.module';
 
 @NgModule({
   declarations: [SignupPageComponent, SignupFormComponent],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    ReactiveFormsModule,
-    SignupRoutingModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, SignupRoutingModule],
 })
 export class SignupModule {}
